1f38b6fc3359a36afdab69eab9c25f7a3bc8a038
Hyeonil Na <sjrnfl528@gmail.com>
2021-11-11 11:26:18 +0900

    초간단 사이트 구축

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello, Heroku"
+    return render_template("index.html")
+
+
+@app.route("/profile")
+def profile():
+    # DB Access
+    like_foods = [
+        "삼겹살",
+        "국밥",
+        "김치찌개",
+        "짬뽕",
+        "짜장면",
+    ]
+    return render_template("profile.html", like_foods=like_foods)
+
+
+@app.route("/posts")
+def post_list():
+    return render_template("post_list.html")
